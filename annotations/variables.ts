cd3a637fbd91a88deae60ddd6f234bc36492cbcb
SilvesterSpath <spath.sz@gmail.com>
2023-04-19 06:32:50 +0200

    json returns any type

--- annotations/variables.ts
+++ annotations/variables.ts
@@ -27,6 +27,12 @@ let point: { x: number; y: number } = {
 };
 
 // Function
-const logNumber: (i: number) => void = (i) => {
+const logNumber: (i: number) => void = (i: number) => {
   console.log(i);
 };
+
+// When to use annotations
+// 1) Function that returns the 'any' type
+const json = '{"x": 10, "y": 20}';
+const coordinates = JSON.parse(json);
+console.log(coordinates); // {x: 10, y: 20};
