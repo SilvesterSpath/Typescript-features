c0d6bf39b90af8af54e2c86b811d44a0197bfb44
SilvesterSpath <spath.sz@gmail.com>
2023-04-20 06:27:14 +0200

    higher order functions

--- arrays.ts
+++ arrays.ts
@@ -1,9 +1,19 @@
 const carMakers0: string[] = [];
 const carMakers = ['ford', 'toyota', 'chevy'];
 
-carMakers.push('mercedes');
-
 const dates = [new Date(), new Date()];
 
 const carByMake0: string[][] = [];
 const carsByMake = [['f150', 'f140'], ['corolla'], ['camaro']];
+
+// Help with infernce when extracting values
+const lastMaker = carMakers.pop();
+const car = carMakers[0];
+
+// Prevent incompatible values
+carMakers.push('mercedes');
+
+// Help with 'map'
+carMakers.map((item: string): string => {
+  return item;
+});
