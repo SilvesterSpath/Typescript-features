97f81f39047294dc1ad091b2c98ff24b3ce93c47
SilvesterSpath <spath.sz@gmail.com>
2023-04-20 05:59:48 +0200

    array inference

---
+++ arrays.ts
@@ -0,0 +1,9 @@
+const carMakers0: string[] = [];
+const carMakers = ['ford', 'toyota', 'chevy'];
+
+carMakers.push('mercedes');
+
+const dates = [new Date(), new Date()];
+
+const carByMake0: string[][] = [];
+const carsByMake = [['f150', 'f140'], ['corolla'], ['camaro']];
