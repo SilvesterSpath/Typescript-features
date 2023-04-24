980f59d49ed298916390ff5b87eec2289234d600
SilvesterSpath <spath.sz@gmail.com>
2023-04-24 06:06:50 +0200

    interfaces

---
+++ interfaces.ts
@@ -0,0 +1,20 @@
+// generic name for an interface
+interface Vehicle {
+  name: string;
+  year: number;
+  broken: boolean;
+}
+
+const oldCivic = {
+  name: 'civic',
+  year: 2000,
+  broken: true,
+};
+
+const printVehicle = (vehicle: Vehicle): void => {
+  console.log(`Name: ${vehicle.name}`);
+  console.log(`Year: ${vehicle.year}`);
+  console.log(`Broken?: ${vehicle.broken}`);
+};
+
+printVehicle(oldCivic);
