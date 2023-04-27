714b0b79a408966faf4ab1413262f6e5918f96f3
SilvesterSpath <spath.sz@gmail.com>
2023-04-27 06:34:49 +0200

    classes 4

--- classes3.ts
+++ classes3.ts
@@ -1,8 +1,21 @@
 class Vehicle3 {
+  // either here initialize a property or in the constructor (this is declaration)
+  // color: string;
+
+  // constructor(color: string = 'black') {
+  //  this.color = color;
+  //}
+
+  // this is a shorter version for the code above
+  constructor(public color: string = 'black') {}
+
   protected honk(): void {
     console.log('beep');
   }
 }
+
+const vehicle = new Vehicle3('orange');
+console.log(vehicle.color);
 
 // Inheritance (this is a child class)
 class Car3 extends Vehicle3 {
